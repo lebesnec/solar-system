b1465133f06e531b7972e47ffcde68b03d021f0a
Christophe Le Besnerais <christophe.lebesnerais@gmail.com>
2021-05-24 17:40:04 +0200

    uranus moon

--- src/app/scene/data/Uranus.data.ts
+++ src/app/scene/data/Uranus.data.ts
@@ -1,5 +1,491 @@
 import {CELESTIAL_BODY_TYPE, CelestialBody} from '../scene.model';
 
+export const CORDELIA: CelestialBody = {
+  id: 'cordelia',
+  position: {
+    x: 0,
+    y: 0
+  },
+  speed: 0, // TODO
+  mass: 4.4e16,
+  radius: 20,
+  semiMajorAxis: 49770,
+  eccentricity: 0.00026,
+  trueAnomaly: 0,
+  meanAnomaly: 0, // TODO
+  type: CELESTIAL_BODY_TYPE.SATELLITE,
+  satellites: [],
+  orbitBody: null
+};
+
+export const OPHELIA: CelestialBody = {
+  id: 'ophelia',
+  position: {
+    x: 0,
+    y: 0
+  },
+  speed: 0, // TODO
+  mass: 5.3e16,
+  radius: 21.5,
+  semiMajorAxis: 53790,
+  eccentricity: 0.00992,
+  trueAnomaly: 0,
+  meanAnomaly: 0, // TODO
+  type: CELESTIAL_BODY_TYPE.SATELLITE,
+  satellites: [],
+  orbitBody: null
+};
+
+export const BIANCA: CelestialBody = {
+  id: 'bianca',
+  position: {
+    x: 0,
+    y: 0
+  },
+  speed: 0, // TODO
+  mass: 9.2e16,
+  radius: 25.5,
+  semiMajorAxis: 59170,
+  eccentricity: 0.00092,
+  trueAnomaly: 0,
+  meanAnomaly: 0, // TODO
+  type: CELESTIAL_BODY_TYPE.SATELLITE,
+  satellites: [],
+  orbitBody: null
+};
+
+export const CRESSIDA: CelestialBody = {
+  id: 'cressida',
+  position: {
+    x: 0,
+    y: 0
+  },
+  speed: 0, // TODO
+  mass: 34e16,
+  radius: 40,
+  semiMajorAxis: 61780,
+  eccentricity: 0.00036,
+  trueAnomaly: 0,
+  meanAnomaly: 0, // TODO
+  type: CELESTIAL_BODY_TYPE.SATELLITE,
+  satellites: [],
+  orbitBody: null
+};
+
+export const DESDEMONA: CelestialBody = {
+  id: 'desdemona',
+  position: {
+    x: 0,
+    y: 0
+  },
+  speed: 0, // TODO
+  mass: 18e16,
+  radius: 32,
+  semiMajorAxis: 62680,
+  eccentricity: 0.00013,
+  trueAnomaly: 0,
+  meanAnomaly: 0, // TODO
+  type: CELESTIAL_BODY_TYPE.SATELLITE,
+  satellites: [],
+  orbitBody: null
+};
+
+export const JULIET: CelestialBody = {
+  id: 'juliet',
+  position: {
+    x: 0,
+    y: 0
+  },
+  speed: 0, // TODO
+  mass: 56e16,
+  radius: 47,
+  semiMajorAxis: 64350,
+  eccentricity: 0.00066,
+  trueAnomaly: 0,
+  meanAnomaly: 0, // TODO
+  type: CELESTIAL_BODY_TYPE.SATELLITE,
+  satellites: [],
+  orbitBody: null
+};
+
+export const PORTIA: CelestialBody = {
+  id: 'portia',
+  position: {
+    x: 0,
+    y: 0
+  },
+  speed: 0, // TODO
+  mass: 170e16,
+  radius: 67.5,
+  semiMajorAxis: 66090,
+  eccentricity: 0.00005,
+  trueAnomaly: 0,
+  meanAnomaly: 0, // TODO
+  type: CELESTIAL_BODY_TYPE.SATELLITE,
+  satellites: [],
+  orbitBody: null
+};
+
+export const ROSALIND: CelestialBody = {
+  id: 'rosalind',
+  position: {
+    x: 0,
+    y: 0
+  },
+  speed: 0, // TODO
+  mass: 25e16,
+  radius: 36,
+  semiMajorAxis: 69940,
+  eccentricity: 0.00011,
+  trueAnomaly: 0,
+  meanAnomaly: 0, // TODO
+  type: CELESTIAL_BODY_TYPE.SATELLITE,
+  satellites: [],
+  orbitBody: null
+};
+
+export const CUPID: CelestialBody = {
+  id: 'cupid',
+  position: {
+    x: 0,
+    y: 0
+  },
+  speed: 0, // TODO
+  mass: 0.38e16,
+  radius: 9,
+  semiMajorAxis: 74800,
+  eccentricity: 0.0013,
+  trueAnomaly: 0,
+  meanAnomaly: 0, // TODO
+  type: CELESTIAL_BODY_TYPE.SATELLITE,
+  satellites: [],
+  orbitBody: null
+};
+
+export const BELINDA: CelestialBody = {
+  id: 'belinda',
+  position: {
+    x: 0,
+    y: 0
+  },
+  speed: 0, // TODO
+  mass: 49e16,
+  radius: 45,
+  semiMajorAxis: 75260,
+  eccentricity: 0.00007,
+  trueAnomaly: 0,
+  meanAnomaly: 0, // TODO
+  type: CELESTIAL_BODY_TYPE.SATELLITE,
+  satellites: [],
+  orbitBody: null
+};
+
+export const PERDITA: CelestialBody = {
+  id: 'perdita',
+  position: {
+    x: 0,
+    y: 0
+  },
+  speed: 0, // TODO
+  mass: 1.8e16,
+  radius: 15,
+  semiMajorAxis: 76400,
+  eccentricity: 0.0012,
+  trueAnomaly: 0,
+  meanAnomaly: 0, // TODO
+  type: CELESTIAL_BODY_TYPE.SATELLITE,
+  satellites: [],
+  orbitBody: null
+};
+
+export const PUCK: CelestialBody = {
+  id: 'puck',
+  position: {
+    x: 0,
+    y: 0
+  },
+  speed: 0, // TODO
+  mass: 290e16,
+  radius: 81,
+  semiMajorAxis: 86010,
+  eccentricity: 0.00012,
+  trueAnomaly: 0,
+  meanAnomaly: 0, // TODO
+  type: CELESTIAL_BODY_TYPE.SATELLITE,
+  satellites: [],
+  orbitBody: null
+};
+
+export const MAB: CelestialBody = {
+  id: 'mab',
+  position: {
+    x: 0,
+    y: 0
+  },
+  speed: 0, // TODO
+  mass: 1.0e16,
+  radius: 12.5,
+  semiMajorAxis: 97700,
+  eccentricity: 0.0025,
+  trueAnomaly: 0,
+  meanAnomaly: 0, // TODO
+  type: CELESTIAL_BODY_TYPE.SATELLITE,
+  satellites: [],
+  orbitBody: null
+};
+
+export const MIRANDA: CelestialBody = {
+  id: 'miranda',
+  position: {
+    x: 0,
+    y: 0
+  },
+  speed: 0, // TODO
+  mass: 6590e16,
+  radius: 235.8,
+  semiMajorAxis: 129390,
+  eccentricity: 0.0013,
+  trueAnomaly: 0,
+  meanAnomaly: 0, // TODO
+  type: CELESTIAL_BODY_TYPE.SATELLITE,
+  satellites: [],
+  orbitBody: null
+};
+
+export const ARIEL: CelestialBody = {
+  id: 'ariel',
+  position: {
+    x: 0,
+    y: 0
+  },
+  speed: 0, // TODO
+  mass: 135300e16,
+  radius: 578.9,
+  semiMajorAxis: 191020,
+  eccentricity: 0.0012,
+  trueAnomaly: 0,
+  meanAnomaly: 0, // TODO
+  type: CELESTIAL_BODY_TYPE.SATELLITE,
+  satellites: [],
+  orbitBody: null
+};
+
+export const UMBRIEL: CelestialBody = {
+  id: 'umbriel',
+  position: {
+    x: 0,
+    y: 0
+  },
+  speed: 0, // TODO
+  mass: 117200e16,
+  radius: 584.7,
+  semiMajorAxis: 266300,
+  eccentricity: 0.0039,
+  trueAnomaly: 0,
+  meanAnomaly: 0, // TODO
+  type: CELESTIAL_BODY_TYPE.SATELLITE,
+  satellites: [],
+  orbitBody: null
+};
+
+export const TITANIA: CelestialBody = {
+  id: 'titania',
+  position: {
+    x: 0,
+    y: 0
+  },
+  speed: 0, // TODO
+  mass: 352700e16,
+  radius: 788.4,
+  semiMajorAxis: 435910,
+  eccentricity: 0.0011,
+  trueAnomaly: 0,
+  meanAnomaly: 0, // TODO
+  type: CELESTIAL_BODY_TYPE.SATELLITE,
+  satellites: [],
+  orbitBody: null
+};
+
+export const OBERON: CelestialBody = {
+  id: 'oberon',
+  position: {
+    x: 0,
+    y: 0
+  },
+  speed: 0, // TODO
+  mass: 301400e16,
+  radius: 761.4,
+  semiMajorAxis: 583520,
+  eccentricity: 0.0014,
+  trueAnomaly: 0,
+  meanAnomaly: 0, // TODO
+  type: CELESTIAL_BODY_TYPE.SATELLITE,
+  satellites: [],
+  orbitBody: null
+};
+
+export const FRANCISCO: CelestialBody = {
+  id: 'francisco',
+  position: {
+    x: 0,
+    y: 0
+  },
+  speed: 0, // TODO
+  mass: 0.72e16,
+  radius: 11,
+  semiMajorAxis: 4282900,
+  eccentricity: 0.1324,
+  trueAnomaly: 0,
+  meanAnomaly: 0, // TODO
+  type: CELESTIAL_BODY_TYPE.SATELLITE,
+  satellites: [],
+  orbitBody: null
+};
+
+export const CALIBAN: CelestialBody = {
+  id: 'caliban',
+  position: {
+    x: 0,
+    y: 0
+  },
+  speed: 0, // TODO
+  mass: 7.0e16,
+  radius: 21,
+  semiMajorAxis: 7231100,
+  eccentricity: 0.1812,
+  trueAnomaly: 0,
+  meanAnomaly: 0, // TODO
+  type: CELESTIAL_BODY_TYPE.SATELLITE,
+  satellites: [],
+  orbitBody: null
+};
+
+export const STEPHANO: CelestialBody = {
+  id: 'stephano',
+  position: {
+    x: 0,
+    y: 0
+  },
+  speed: 0, // TODO
+  mass: 2.2e16,
+  radius: 16,
+  semiMajorAxis: 8007400,
+  eccentricity: 0.2248,
+  trueAnomaly: 0,
+  meanAnomaly: 0, // TODO
+  type: CELESTIAL_BODY_TYPE.SATELLITE,
+  satellites: [],
+  orbitBody: null
+};
+
+export const TRINCULO: CelestialBody = {
+  id: 'trinculo',
+  position: {
+    x: 0,
+    y: 0
+  },
+  speed: 0, // TODO
+  mass: 0.39e16,
+  radius: 9,
+  semiMajorAxis: 8505200,
+  eccentricity: 0.2194,
+  trueAnomaly: 0,
+  meanAnomaly: 0, // TODO
+  type: CELESTIAL_BODY_TYPE.SATELLITE,
+  satellites: [],
+  orbitBody: null
+};
+
+export const SYCORAX: CelestialBody = {
+  id: 'sycorax',
+  position: {
+    x: 0,
+    y: 0
+  },
+  speed: 0, // TODO
+  mass: 230e16,
+  radius: 78.5,
+  semiMajorAxis: 12179400,
+  eccentricity: 0.5219,
+  trueAnomaly: 0,
+  meanAnomaly: 0, // TODO
+  type: CELESTIAL_BODY_TYPE.SATELLITE,
+  satellites: [],
+  orbitBody: null
+};
+
+export const MARGARET: CelestialBody = {
+  id: 'margaret',
+  position: {
+    x: 0,
+    y: 0
+  },
+  speed: 0, // TODO
+  mass: 0.54e16,
+  radius: 10,
+  semiMajorAxis: 14146700,
+  eccentricity: 0.6772,
+  trueAnomaly: 0,
+  meanAnomaly: 0, // TODO
+  type: CELESTIAL_BODY_TYPE.SATELLITE,
+  satellites: [],
+  orbitBody: null
+};
+
+export const PROSPERO: CelestialBody = {
+  id: 'prospero',
+  position: {
+    x: 0,
+    y: 0
+  },
+  speed: 0, // TODO
+  mass: 8.5e16,
+  radius: 25,
+  semiMajorAxis: 16276800,
+  eccentricity: 0.4445,
+  trueAnomaly: 0,
+  meanAnomaly: 0, // TODO
+  type: CELESTIAL_BODY_TYPE.SATELLITE,
+  satellites: [],
+  orbitBody: null
+};
+
+export const SETEBOS: CelestialBody = {
+  id: 'setebos',
+  position: {
+    x: 0,
+    y: 0
+  },
+  speed: 0, // TODO
+  mass: 7.5e16,
+  radius: 24,
+  semiMajorAxis: 17420400,
+  eccentricity: 0.5908,
+  trueAnomaly: 0,
+  meanAnomaly: 0, // TODO
+  type: CELESTIAL_BODY_TYPE.SATELLITE,
+  satellites: [],
+  orbitBody: null
+};
+
+export const FERDINAND: CelestialBody = {
+  id: 'ferdinand',
+  position: {
+    x: 0,
+    y: 0
+  },
+  speed: 0, // TODO
+  mass: 0.54e16,
+  radius: 10,
+  semiMajorAxis: 20430000,
+  eccentricity: 0.3993,
+  trueAnomaly: 0,
+  meanAnomaly: 0, // TODO
+  type: CELESTIAL_BODY_TYPE.SATELLITE,
+  satellites: [],
+  orbitBody: null
+};
+
 export const URANUS: CelestialBody = {
   id: 'uranus',
   position: {
@@ -14,6 +500,36 @@ export const URANUS: CelestialBody = {
   trueAnomaly: 0,
   meanAnomaly: 142.2386,
   type: CELESTIAL_BODY_TYPE.PLANET,
-  satellites: [],
+  satellites: [
+    CORDELIA,
+    OPHELIA,
+    BIANCA,
+    CRESSIDA,
+    DESDEMONA,
+    JULIET,
+    PORTIA,
+    ROSALIND,
+    CUPID,
+    BELINDA,
+    PERDITA,
+    PUCK,
+    MAB,
+    MIRANDA,
+    ARIEL,
+    UMBRIEL,
+    TITANIA,
+    OBERON,
+    FRANCISCO,
+    CALIBAN,
+    STEPHANO,
+    TRINCULO,
+    SYCORAX,
+    MARGARET,
+    PROSPERO,
+    SETEBOS,
+    FERDINAND
+  ],
   orbitBody: null
 };
+
+URANUS.satellites.forEach(satellite => satellite.orbitBody = URANUS);
