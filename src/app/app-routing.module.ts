3bf03da346b00eedde473fe2fcc41b64258cc485
Christophe Le Besnerais <christophe.lebesnerais@gmail.com>
2021-07-07 17:53:20 +0200

    shell

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,10 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import {SceneComponent} from './scene/scene.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '**', component: SceneComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
