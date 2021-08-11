5dd79794c8ec8192fc2447356dc701fdb510a7ac
Christophe Le Besnerais <christophe.lebesnerais@gmail.com>
2021-08-11 18:17:05 +0200

    no overlapping label

--- src/app/scene/scene.component.ts
+++ src/app/scene/scene.component.ts
@@ -19,7 +19,8 @@ const SCALE_PLANET = 3.0;
 const SCALE_SMALL_BODY = 6.0;
 
 const MIN_BODY_RADIUS = 50; // km
-const LABEL_DISTANCE: Point = { x: 20, y: 20 }; // px
+const LABEL_DISTANCE_TO_BODY: Point = { x: 20, y: 20 }; // px
+const LABEL_SPACING = 5; // px
 const LABEL_TRANSITION_MS = 50; // ms
 const LABEL_PATH_MARGIN = 4; // px
 const ZOOM_TRANSITION_MS = 500; // ms
@@ -139,13 +140,13 @@ export class SceneComponent implements AfterViewInit {
                                             .attr('id', (d) => 'labeltext_' + d.body.id)
                                             .attr('class', (d) => 'label ' + d.body.type + ' ' + d.body.id)
                                             .text((d) => d.body.id)
-                                            .attr('x', (d) => d.boundingBox.right + LABEL_DISTANCE.x)
-                                            .attr('y', (d) => d.boundingBox.bottom + LABEL_DISTANCE.y)
+                                            .attr('x', (d) => d.boundingBox.right + LABEL_DISTANCE_TO_BODY.x)
+                                            .attr('y', (d) => d.boundingBox.bottom + LABEL_DISTANCE_TO_BODY.y)
                                             .on('mouseover', (event, d) => {
                                               const textBoundingBox = event.currentTarget.getBoundingClientRect();
                                               this.labelsPath.attr('d', `M ${d.boundingBox.x + (d.boundingBox.width / 2)} ${d.boundingBox.y + (d.boundingBox.height / 2)}
-                                                                         L ${d.boundingBox.right + LABEL_DISTANCE.x - LABEL_PATH_MARGIN} ${d.boundingBox.bottom + LABEL_DISTANCE.y + LABEL_PATH_MARGIN}
-                                                                         L ${d.boundingBox.right + LABEL_DISTANCE.x + textBoundingBox.width + LABEL_PATH_MARGIN} ${d.boundingBox.bottom + LABEL_DISTANCE.y + LABEL_PATH_MARGIN}`)
+                                                                         L ${d.boundingBox.right + LABEL_DISTANCE_TO_BODY.x - LABEL_PATH_MARGIN} ${d.boundingBox.bottom + LABEL_DISTANCE_TO_BODY.y + LABEL_PATH_MARGIN}
+                                                                         L ${d.boundingBox.right + LABEL_DISTANCE_TO_BODY.x + textBoundingBox.width + LABEL_PATH_MARGIN} ${d.boundingBox.bottom + LABEL_DISTANCE_TO_BODY.y + LABEL_PATH_MARGIN}`)
                                                           .transition()
                                                           .duration(LABEL_TRANSITION_MS)
                                                           .style('opacity', 1);
@@ -169,8 +170,8 @@ export class SceneComponent implements AfterViewInit {
                                                                 .call(this.d3Zoom.transform, zoomTo);
 
                                             }),
-                              update => update.attr('x', (d) => d.boundingBox.right + LABEL_DISTANCE.x)
-                                              .attr('y', (d) =>  d.boundingBox.bottom + LABEL_DISTANCE.y)
+                              update => update.attr('x', (d) => d.boundingBox.right + LABEL_DISTANCE_TO_BODY.x)
+                                              .attr('y', (d) =>  d.boundingBox.bottom + LABEL_DISTANCE_TO_BODY.y)
                             );
     this.arrangeLabels();
   }
@@ -190,10 +191,20 @@ export class SceneComponent implements AfterViewInit {
           const label2 = nodes2[i2];
           if (label1 !== label2) {
             const bb2 = label2.getBoundingClientRect();
-            if ((Math.abs(bb1.left - bb2.left) * 2 < (bb1.width + bb2.width)) && (Math.abs(bb1.top - bb2.top) * 2 < (bb1.height + bb2.height))) {
+            // if (d1.body.id === 'earth' && d2.body.id === 'mars') {
+            //   console.log(bb1, bb2);
+            //   // console.log(bb1.left - LABEL_SPACING < bb2.right + LABEL_SPACING);
+            //   // console.log(bb1.right + LABEL_SPACING > bb2.left - LABEL_SPACING);
+            //   console.log(bb1.top, bb2.bottom, bb1.top - LABEL_SPACING, bb2.bottom + LABEL_SPACING, bb1.top - LABEL_SPACING > bb2.bottom + LABEL_SPACING);
+            //   // console.log(bb1.bottom - LABEL_SPACING < bb2.top + LABEL_SPACING);
+            // }
+            if (bb1.left - LABEL_SPACING < bb2.right + LABEL_SPACING &&
+                bb1.right + LABEL_SPACING > bb2.left - LABEL_SPACING &&
+                bb1.top - LABEL_SPACING < bb2.bottom + LABEL_SPACING &&
+                bb1.bottom + LABEL_SPACING > bb2.top - LABEL_SPACING) {
               // overlap, move labels
-              const dx = (Math.max(0, bb1.right - bb2.left) + Math.min(0, bb1.left - bb2.right)) * 0.01;
-              const dy = (Math.max(0, bb1.bottom - bb2.top) + Math.min(0, bb1.top - bb2.bottom)) * 0.02;
+              const dx = (Math.max(0, bb1.right - bb2.left) + Math.min(0, bb1.left - bb2.right)) * 0.05;
+              const dy = (Math.max(0, bb1.bottom - bb2.top) + Math.min(0, bb1.top - bb2.bottom)) * 0.05;
               move += Math.abs(dx) + Math.abs(dy);
 
               select(label1).attr('x', +select(label1).attr('x') + dx).attr('y', +select(label1).attr('y') + dy);
