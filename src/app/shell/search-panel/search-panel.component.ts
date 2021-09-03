5ca4f59295e922ee1b1561030e23146af65c8232
Christophe Le Besnerais <christophe.lebesnerais@gmail.com>
2021-09-03 18:05:09 +0200

    opti search

--- src/app/shell/search-panel/search-panel.component.ts
+++ src/app/shell/search-panel/search-panel.component.ts
@@ -1,19 +1,23 @@
-import {Component, Input} from '@angular/core';
+import {Component, Input, OnChanges, OnInit, SimpleChanges} from '@angular/core';
 import { DWARF_PLANETS } from 'src/app/scene/data/DwarfPlanets.data';
 import {SOLAR_SYSTEM, SUN} from 'src/app/scene/data/SolarSystem.data';
 import {CelestialBody} from '../../scene/scene.model';
 import {SearchPanelService} from './search-panel.service';
 import {GANYMEDE, JUPITER} from '../../scene/data/Jupiter.data';
 import {MOON} from '../../scene/data/Earth.data';
+import {Subject} from 'rxjs';
+import {debounceTime} from 'rxjs/operators';
 
 @Component({
   selector: 'app-search-panel',
   templateUrl: './search-panel.component.html',
   styleUrls: ['./search-panel.component.scss']
 })
-export class SearchPanelComponent {
+export class SearchPanelComponent implements OnInit, OnChanges {
 
   @Input() public search = '';
+
+  public searchResult: CelestialBody[] = null;
 
   public readonly SUN = SUN;
   public readonly DWARF_PLANETS = DWARF_PLANETS;
@@ -21,13 +25,26 @@ export class SearchPanelComponent {
   public readonly MOON = MOON;
   public readonly GANYMEDE = GANYMEDE;
 
+  private searchChanged: Subject<void> = new Subject<void>();
+
   constructor(
     private searchService: SearchPanelService
   ) { }
 
-  public get searchResult(): CelestialBody[] {
-    // TODO called to often
-    return this.searchService.filter<CelestialBody>(SOLAR_SYSTEM, [ 'id' ], this.search);
+  public ngOnInit(): void {
+    this.searchChanged.pipe(debounceTime(300)).subscribe((search) => {
+      if (this.search === '') {
+        this.searchResult = null;
+      } else {
+        this.searchResult = this.searchService.filter(SOLAR_SYSTEM, ['id'], this.search);
+      }
+    });
+  }
+
+  public ngOnChanges(changes: SimpleChanges): void {
+    if (changes.search) {
+      this.searchChanged.next();
+    }
   }
 
   public onBodySelected(body: CelestialBody): void {
