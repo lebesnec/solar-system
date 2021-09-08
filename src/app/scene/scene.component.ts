c9948f5260ea174cf3bcbc9a25f5c91d2b9a17a1
Christophe Le Besnerais <christophe.lebesnerais@gmail.com>
2021-09-08 17:19:24 +0200

    show all

--- src/app/scene/scene.component.ts
+++ src/app/scene/scene.component.ts
@@ -16,10 +16,8 @@ import {URANUS} from './data/Uranus.data';
 import {NEPTUNE} from './data/Neptune.data';
 
 const NB_POINTS_ORBIT = 90;
-const HIDDEN_THRESHOLD = 10.0;
 const MIN_BODY_RADIUS = 50; // km
 const LABEL_DISTANCE_TO_BODY: Point = { x: 20, y: 20 }; // px
-const LABEL_SPACING = 5; // px
 const LABEL_TRANSITION_MS = 50; // ms
 const LABEL_PATH_MARGIN = 4; // px
 const ZOOM_TRANSITION_MS = 500; // ms
@@ -75,10 +73,6 @@ export class SceneComponent implements OnInit, AfterViewInit {
   private initZoom(): void {
     this.d3Zoom = zoom().on('zoom', (e) => {
       this.scale = e.transform.k;
-
-      this.groupZoomableSelection.selectAll('.celestial-body').classed('hidden', (body) => body.radius * this.scale <= HIDDEN_THRESHOLD);
-      this.groupZoomableSelection.selectAll('.orbit').classed('hidden', (orbit) => orbit.body.radius * this.scale <= HIDDEN_THRESHOLD);
-      this.groupStaticSelection.selectAll('.label').classed('hidden', (orbit) => orbit.body.radius * this.scale <= HIDDEN_THRESHOLD);
 
       this.groupZoomableSelection.attr('transform', e.transform);
       this.initLabels();
@@ -141,7 +135,6 @@ export class SceneComponent implements OnInit, AfterViewInit {
                               enter => enter.append('text')
                                             .attr('id', (d) => 'labeltext_' + d.body.id)
                                             .attr('class', (d) => 'label ' + d.body.type + ' ' + d.body.id)
-                                            .classed('hidden', (d) => d.body.radius * this.scale <= HIDDEN_THRESHOLD)
                                             .text((d) => d.body.id)
                                             .attr('x', (d) => d.boundingBox.right + LABEL_DISTANCE_TO_BODY.x)
                                             .attr('y', (d) => d.boundingBox.bottom + LABEL_DISTANCE_TO_BODY.y)
@@ -168,12 +161,10 @@ export class SceneComponent implements OnInit, AfterViewInit {
                               update => update.attr('x', (d) => d.boundingBox.right + LABEL_DISTANCE_TO_BODY.x)
                                               .attr('y', (d) =>  d.boundingBox.bottom + LABEL_DISTANCE_TO_BODY.y)
                             );
-    this.arrangeLabels();
   }
 
   private zoomTo(body: CelestialBody): void {
     const bodySelection = select('#' + body.id);
-    bodySelection.classed('hidden', false);
 
     const bbox = (bodySelection.node() as any).getBBox();
     const scale = this.getScale(body);
@@ -186,112 +177,112 @@ export class SceneComponent implements OnInit, AfterViewInit {
                       .duration(ZOOM_TRANSITION_MS)
                       .call(this.d3Zoom.transform, zoomTo);
   }
-
-  private arrangeLabels(): void {
-    const labels = this.groupStaticSelection.selectAll('.label:not(.hidden)');
-    // let move = 1;
-    //
-    // while (move > 0) {
-    //   move = 0;
-    //
-    //   labels.each((d1, i1, nodes1) => {
-    //     const label1 = nodes1[i1];
-    //     let bb1 = label1.getBoundingClientRect();
-    //
-    //     labels.each((d2, i2, nodes2) => {
-    //       const label2 = nodes2[i2];
-    //       if (label1 !== label2) {
-    //         const bb2 = label2.getBoundingClientRect();
-    //         if (bb1.left - LABEL_SPACING < bb2.right + LABEL_SPACING &&
-    //             bb1.right + LABEL_SPACING > bb2.left - LABEL_SPACING &&
-    //             bb1.top - LABEL_SPACING < bb2.bottom + LABEL_SPACING &&
-    //             bb1.bottom + LABEL_SPACING > bb2.top - LABEL_SPACING) {
-    //           // overlap, move labels
-    //           const dx = (Math.max(0, bb1.right - bb2.left) + Math.min(0, bb1.left - bb2.right)) * 0.2;
-    //           const dy = (Math.max(0, bb1.bottom - bb2.top) + Math.min(0, bb1.top - bb2.bottom)) * 0.2;
-    //           move += Math.abs(dx) + Math.abs(dy);
-    //
-    //           const selectionLabel1 = select(label1);
-    //           selectionLabel1.attr('x', +selectionLabel1.attr('x') + dx).attr('y', +selectionLabel1.attr('y') + dy);
-    //           const selectionLabel2 = select(label2);
-    //           selectionLabel2.attr('x', +selectionLabel2.attr('x') - dx).attr('y', +selectionLabel2.attr('y') - dy);
-    //           bb1 = label1.getBoundingClientRect();
-    //         }
-    //       }
-    //     });
-    //   });
-    // }
-
-    labels.each((d1, i1, nodes1) => {
-      const label1 = nodes1[i1];
-      const bb1 = label1.getBoundingClientRect();
-
-      labels.each((d2, i2, nodes2) => {
-        const label2 = nodes2[i2];
-        if (label1 !== label2) {
-          const bb2 = label2.getBoundingClientRect();
-          if (bb1.left - LABEL_SPACING < bb2.right + LABEL_SPACING &&
-              bb1.right + LABEL_SPACING > bb2.left - LABEL_SPACING &&
-              bb1.top - LABEL_SPACING < bb2.bottom + LABEL_SPACING &&
-              bb1.bottom + LABEL_SPACING > bb2.top - LABEL_SPACING) {
-            // overlap, move labels
-            if (d1.body.radius > d2.body.radius) {
-              select(label2).classed('hidden', true);
-            } else {
-              select(label1).classed('hidden', true);
-            }
-          }
-        }
-      });
-    });
-  }
-
-  // private arrangeLabels(): any[] {
-  //   const labelsData = SOLAR_SYSTEM.map((body) => {
-  //     const boundingBox = (select('#' + body.id).node() as any).getBoundingClientRect(); // TODO store node
-  //     return {
-  //       body,
-  //       x: boundingBox.right + LABEL_DISTANCE_TO_BODY.x,
-  //       y: boundingBox.bottom + LABEL_DISTANCE_TO_BODY.y,
-  //       boundingBox
-  //     };
-  //   });
   //
-  //   let move = 1;
+  // private arrangeLabels(): void {
+  //   const labels = this.groupStaticSelection.selectAll('.label:not(.hidden)');
+  //   // let move = 1;
+  //   //
+  //   // while (move > 0) {
+  //   //   move = 0;
+  //   //
+  //   //   labels.each((d1, i1, nodes1) => {
+  //   //     const label1 = nodes1[i1];
+  //   //     let bb1 = label1.getBoundingClientRect();
+  //   //
+  //   //     labels.each((d2, i2, nodes2) => {
+  //   //       const label2 = nodes2[i2];
+  //   //       if (label1 !== label2) {
+  //   //         const bb2 = label2.getBoundingClientRect();
+  //   //         if (bb1.left - LABEL_SPACING < bb2.right + LABEL_SPACING &&
+  //   //             bb1.right + LABEL_SPACING > bb2.left - LABEL_SPACING &&
+  //   //             bb1.top - LABEL_SPACING < bb2.bottom + LABEL_SPACING &&
+  //   //             bb1.bottom + LABEL_SPACING > bb2.top - LABEL_SPACING) {
+  //   //           // overlap, move labels
+  //   //           const dx = (Math.max(0, bb1.right - bb2.left) + Math.min(0, bb1.left - bb2.right)) * 0.2;
+  //   //           const dy = (Math.max(0, bb1.bottom - bb2.top) + Math.min(0, bb1.top - bb2.bottom)) * 0.2;
+  //   //           move += Math.abs(dx) + Math.abs(dy);
+  //   //
+  //   //           const selectionLabel1 = select(label1);
+  //   //           selectionLabel1.attr('x', +selectionLabel1.attr('x') + dx).attr('y', +selectionLabel1.attr('y') + dy);
+  //   //           const selectionLabel2 = select(label2);
+  //   //           selectionLabel2.attr('x', +selectionLabel2.attr('x') - dx).attr('y', +selectionLabel2.attr('y') - dy);
+  //   //           bb1 = label1.getBoundingClientRect();
+  //   //         }
+  //   //       }
+  //   //     });
+  //   //   });
+  //   // }
   //
-  //   while (move > 0) {
-  //     move = 0;
+  //   labels.each((d1, i1, nodes1) => {
+  //     const label1 = nodes1[i1];
+  //     const bb1 = label1.getBoundingClientRect();
   //
-  //     labelsData.forEach(label1 => {
-  //       labelsData.forEach(label2 => {
-  //         if (label1 !== label2) {
-  //           // if (d1.body.id === 'earth' && d2.body.id === 'mars') {
-  //           //   console.log(bb1, bb2);
-  //           //   // console.log(bb1.left - LABEL_SPACING < bb2.right + LABEL_SPACING);
-  //           //   // console.log(bb1.right + LABEL_SPACING > bb2.left - LABEL_SPACING);
-  //           //   console.log(bb1.top, bb2.bottom, bb1.top - LABEL_SPACING, bb2.bottom + LABEL_SPACING, bb1.top - LABEL_SPACING > bb2.bottom + LABEL_SPACING);
-  //           //   // console.log(bb1.bottom - LABEL_SPACING < bb2.top + LABEL_SPACING);
-  //           // }
-  //           if (label1.x - LABEL_SPACING < label2.x + LABEL_MAX_WIDTH + LABEL_SPACING &&
-  //             label1.x + LABEL_MAX_WIDTH + LABEL_SPACING > label2.x - LABEL_SPACING &&
-  //             label1.y - LABEL_SPACING < label2.y + LABEL_HEIGHT + LABEL_SPACING &&
-  //             label1.y + LABEL_HEIGHT + LABEL_SPACING > label2.y - LABEL_SPACING) {
-  //             // overlap, move labels
-  //             const dy = (Math.max(0, label1.y + LABEL_HEIGHT - label2.y) + Math.min(0, label1.y - label2.y - LABEL_HEIGHT)) * 0.05;
-  //             move += Math.abs(dy);
-  //
-  //             // select(label1).attr('x', +select(label1).attr('x') + dx).attr('y', +select(label1).attr('y') + dy);
-  //             // select(label2).attr('x', +select(label2).attr('x') - dx).attr('y', +select(label2).attr('y') - dy);
-  //             label1.y = label1.y + dy;
-  //             label2.y = label2.y - dy;
+  //     labels.each((d2, i2, nodes2) => {
+  //       const label2 = nodes2[i2];
+  //       if (label1 !== label2) {
+  //         const bb2 = label2.getBoundingClientRect();
+  //         if (bb1.left - LABEL_SPACING < bb2.right + LABEL_SPACING &&
+  //             bb1.right + LABEL_SPACING > bb2.left - LABEL_SPACING &&
+  //             bb1.top - LABEL_SPACING < bb2.bottom + LABEL_SPACING &&
+  //             bb1.bottom + LABEL_SPACING > bb2.top - LABEL_SPACING) {
+  //           // overlap, move labels
+  //           if (d1.body.radius > d2.body.radius) {
+  //             select(label2).classed('hidden', true);
+  //           } else {
+  //             select(label1).classed('hidden', true);
   //           }
   //         }
-  //       });
+  //       }
   //     });
-  //   }
-  //
-  //   return labelsData;
+  //   });
   // }
+  //
+  // // private arrangeLabels(): any[] {
+  // //   const labelsData = SOLAR_SYSTEM.map((body) => {
+  // //     const boundingBox = (select('#' + body.id).node() as any).getBoundingClientRect(); // TODO store node
+  // //     return {
+  // //       body,
+  // //       x: boundingBox.right + LABEL_DISTANCE_TO_BODY.x,
+  // //       y: boundingBox.bottom + LABEL_DISTANCE_TO_BODY.y,
+  // //       boundingBox
+  // //     };
+  // //   });
+  // //
+  // //   let move = 1;
+  // //
+  // //   while (move > 0) {
+  // //     move = 0;
+  // //
+  // //     labelsData.forEach(label1 => {
+  // //       labelsData.forEach(label2 => {
+  // //         if (label1 !== label2) {
+  // //           // if (d1.body.id === 'earth' && d2.body.id === 'mars') {
+  // //           //   console.log(bb1, bb2);
+  // //           //   // console.log(bb1.left - LABEL_SPACING < bb2.right + LABEL_SPACING);
+  // //           //   // console.log(bb1.right + LABEL_SPACING > bb2.left - LABEL_SPACING);
+  // //           //   console.log(bb1.top, bb2.bottom, bb1.top - LABEL_SPACING, bb2.bottom + LABEL_SPACING, bb1.top - LABEL_SPACING > bb2.bottom + LABEL_SPACING);
+  // //           //   // console.log(bb1.bottom - LABEL_SPACING < bb2.top + LABEL_SPACING);
+  // //           // }
+  // //           if (label1.x - LABEL_SPACING < label2.x + LABEL_MAX_WIDTH + LABEL_SPACING &&
+  // //             label1.x + LABEL_MAX_WIDTH + LABEL_SPACING > label2.x - LABEL_SPACING &&
+  // //             label1.y - LABEL_SPACING < label2.y + LABEL_HEIGHT + LABEL_SPACING &&
+  // //             label1.y + LABEL_HEIGHT + LABEL_SPACING > label2.y - LABEL_SPACING) {
+  // //             // overlap, move labels
+  // //             const dy = (Math.max(0, label1.y + LABEL_HEIGHT - label2.y) + Math.min(0, label1.y - label2.y - LABEL_HEIGHT)) * 0.05;
+  // //             move += Math.abs(dy);
+  // //
+  // //             // select(label1).attr('x', +select(label1).attr('x') + dx).attr('y', +select(label1).attr('y') + dy);
+  // //             // select(label2).attr('x', +select(label2).attr('x') - dx).attr('y', +select(label2).attr('y') - dy);
+  // //             label1.y = label1.y + dy;
+  // //             label2.y = label2.y - dy;
+  // //           }
+  // //         }
+  // //       });
+  // //     });
+  // //   }
+  // //
+  // //   return labelsData;
+  // // }
 
   private getScale(body: CelestialBody): number {
     switch (body) {
