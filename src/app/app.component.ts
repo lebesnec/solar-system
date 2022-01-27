59026836fcd341708eec933cc550aa6e5755aac1
Christophe Le Besnerais <christophe.lebesnerais@gmail.com>
2022-01-27 15:32:47 +0100

    app title i18n

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
-import {TranslateService} from '@ngx-translate/core';
+import {LangChangeEvent, TranslateService} from '@ngx-translate/core';
+import {Title} from '@angular/platform-browser';
 
 @Component({
   selector: 'app-root',
@@ -8,11 +9,21 @@ import {TranslateService} from '@ngx-translate/core';
 })
 export class AppComponent {
 
-  constructor(translate: TranslateService) {
+  constructor(
+    titleService: Title,
+    translate: TranslateService
+  ) {
     // this language will be used as a fallback when a translation isn't found in the current language
     translate.setDefaultLang('en');
     // the lang to use, if the lang isn't available, it will use the current loader to get them
     translate.use('en');
+
+    translate.onLangChange.subscribe((event: LangChangeEvent) => {
+      // Change page title when user changes language preference
+      translate.get('APP_TITLE').subscribe((res: string) => {
+        titleService.setTitle(res);
+      });
+    });
   }
 
 }
